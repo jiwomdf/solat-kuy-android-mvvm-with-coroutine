1e9bfc3b63996f6ab2c29ce788bc35e20d2d3422
jiwomdf <mdf.jiwo@gmail.com>
2020-07-09 01:45:35 +0700

    - add ui testing QuranFragmentTest.kt

--- app/src/androidTest/java/com/programmergabut/solatkuy/ui/fragmentquran/QuranFragmentTest.kt
+++ app/src/androidTest/java/com/programmergabut/solatkuy/ui/fragmentquran/QuranFragmentTest.kt
@@ -3,22 +3,23 @@ package com.programmergabut.solatkuy.ui.fragmentquran
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.programmergabut.solatkuy.R
+import com.programmergabut.solatkuy.ui.MyViewAction
 import com.programmergabut.solatkuy.ui.main.MainActivity
 import com.programmergabut.solatkuy.util.EspressoIdlingResource
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.CoreMatchers.anything
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -76,7 +77,7 @@ class QuranFragmentTest{
         }
 
         onView(withId(R.id.rv_quran_surah)).perform(RecyclerViewActions
-            .actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click())) //click last surah
+            .actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click())) //click first surah
 
         onView(withId(R.id.ab_readQuran)).check(matches(isDisplayed()))
         onView(withId(R.id.tb_readSurah)).check(matches(isDisplayed()))
@@ -91,6 +92,74 @@ class QuranFragmentTest{
             it.findNavController(R.id.navHostFragment).navigate(R.id.quranFragment)
         }
 
+        onView(withId(R.id.s_juzz)).perform(click())
+        onData(anything()).atPosition(1).perform(click())
+
+        onView(withId(R.id.s_juzz)).perform(click())
+        onData(anything()).atPosition(30).perform(click())
+
+        onView(withId(R.id.rv_quran_surah)).perform(RecyclerViewActions
+            .scrollToPosition<RecyclerView.ViewHolder>(37)) //total Al Qur'an juz 30 surah
+    }
+
+    @Test
+    fun test_search_surah(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        activityScenario.onActivity {
+            it.findNavController(R.id.navHostFragment).navigate(R.id.quranFragment)
+        }
+
+        onView(withId(R.id.et_search)).perform(replaceText("Al Faathia"))
+
+        onView(withId(R.id.et_search)).perform(replaceText("Al Baq"))
+
+        onView(withId(R.id.et_search)).perform(replaceText("An Nas"))
+    }
+
+    @Test
+    fun test_like_all_alfathia_ayah(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        activityScenario.onActivity {
+            it.findNavController(R.id.navHostFragment).navigate(R.id.quranFragment)
+        }
+
+        onView(withId(R.id.rv_quran_surah)).perform(RecyclerViewActions
+            .actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click())) //click first surah
+
+        for (i in 0 until 6){
+            onView(withId(R.id.rv_read_surah)).perform(
+                RecyclerViewActions.actionOnItemAtPosition<StaredSurahAdapter.StaredSurahViewHolder>(i,
+                    MyViewAction.clickChildViewWithId(R.id.iv_listFav_fav)))
+        }
 
     }
+
+    @Test
+    fun test_scroll_ayah_been_favorite(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        activityScenario.onActivity {
+            it.findNavController(R.id.navHostFragment).navigate(R.id.quranFragment)
+        }
+
+        onView(withId(R.id.cv_fav_ayah)).perform(click())
+        onView(withId(R.id.rv_fav_ayah)).perform(RecyclerViewActions
+            .scrollToPosition<RecyclerView.ViewHolder>(6))
+    }
+
+    @Test
+    fun test_scroll_then_click_quick_surah(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        activityScenario.onActivity {
+            it.findNavController(R.id.navHostFragment).navigate(R.id.quranFragment)
+        }
+
+        onView(withId(R.id.rv_stared_ayah)).perform(RecyclerViewActions
+            .scrollToPosition<RecyclerView.ViewHolder>(0))
+
+        onView(withId(R.id.rv_stared_ayah)).perform(RecyclerViewActions
+            .actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click())) //click first surah
+    }
+
+
+
 }
