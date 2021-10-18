33db0d4bf9e9c9ea29e7d244765ac81fe4642049
JoeMobarak <mobarak.joe@gmail.com>
2021-10-18 14:33:52 +0300

    Adjust network module and created 2 retrofits using koin

--- app/src/main/java/com/programmergabut/solatkuy/di/NetworkModule.kt
+++ app/src/main/java/com/programmergabut/solatkuy/di/NetworkModule.kt
@@ -3,41 +3,36 @@ package com.programmergabut.solatkuy.di
 import com.google.gson.GsonBuilder
 import com.programmergabut.solatkuy.BuildConfig
 import com.programmergabut.solatkuy.data.remote.api.*
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-object NetworkModule {
+const val AladhanAPI = "AladhanAPI"
+const val QuranAPI = "QuranApi"
 
-    /* Services */
-    @Provides
-    @Singleton
+val networkModule = module {
+
     fun provideGsonConverterFactory(): GsonConverterFactory {
         return GsonConverterFactory.create(GsonBuilder().create())
     }
 
-    @Provides
-    @Singleton
-    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-        .readTimeout(10, TimeUnit.SECONDS)
-        .connectTimeout(10, TimeUnit.SECONDS)
-        .build()
+    fun provideOkHttpClient(): OkHttpClient {
+        val okHttpClientBuilder = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .readTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+        return okHttpClientBuilder.build()
+    }
 
-    @AladhanApi
-    @Provides
-    @Singleton
-    fun provideRetrofitAladhan(gsonConverterFactory: GsonConverterFactory,
-                               okHttpClient: OkHttpClient): Retrofit {
+
+    fun provideRetrofitAladhan(
+        gsonConverterFactory: GsonConverterFactory,
+        okHttpClient: OkHttpClient
+    ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL_ALADHAN)
             .addConverterFactory(gsonConverterFactory)
@@ -45,11 +40,11 @@ object NetworkModule {
             .build()
     }
 
-    @QuranApi
-    @Provides
-    @Singleton
-    fun provideRetrofitQuranApi(gsonConverterFactory: GsonConverterFactory,
-                                okHttpClient: OkHttpClient): Retrofit {
+
+    fun provideRetrofitQuranApi(
+        gsonConverterFactory: GsonConverterFactory,
+        okHttpClient: OkHttpClient
+    ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL_QURAN_API)
             .addConverterFactory(gsonConverterFactory)
@@ -57,33 +52,17 @@ object NetworkModule {
             .build()
     }
 
-    @Provides
-    @Singleton
-    fun provideAsmaAlHusnaService(@AladhanApi retrofit: Retrofit): AsmaAlHusnaService = retrofit.create(
-        AsmaAlHusnaService::class.java)
 
-    @Provides
-    @Singleton
-    fun provideCalendarApiService(@AladhanApi retrofit: Retrofit): PrayerApiService = retrofit.create(
-        PrayerApiService::class.java)
+    single { provideGsonConverterFactory() }
+    single { provideOkHttpClient() }
+    single(named(AladhanAPI)) { provideRetrofitAladhan(get(), get()) }
+    single(named(QuranAPI)) { provideRetrofitQuranApi(get(), get()) }
 
-    @Provides
-    @Singleton
-    fun provideQiblaApiService(@AladhanApi retrofit: Retrofit): QiblaApiService = retrofit.create(
-        QiblaApiService::class.java)
+    single { get<Retrofit>(named(AladhanAPI)).create(AsmaAlHusnaService::class.java) }
+    single { get<Retrofit>(named(AladhanAPI)).create(PrayerApiService::class.java) }
+    single { get<Retrofit>(named(AladhanAPI)).create(QiblaApiService::class.java) }
+    single { get<Retrofit>(named(QuranAPI)).create(AllSurahService::class.java) }
+    single { get<Retrofit>(named(QuranAPI)).create(ReadSurahArService::class.java) }
+    single { get<Retrofit>(named(QuranAPI)).create(ReadSurahEnService::class.java) }
 
-    @Provides
-    @Singleton
-    fun provideAllSurahService(@QuranApi retrofit: Retrofit): AllSurahService = retrofit.create(
-        AllSurahService::class.java)
-
-    @Provides
-    @Singleton
-    fun provideReadSurahArService(@QuranApi retrofit: Retrofit): ReadSurahArService = retrofit.create(
-        ReadSurahArService::class.java)
-
-    @Provides
-    @Singleton
-    fun provideReadSurahEnService(@QuranApi retrofit: Retrofit): ReadSurahEnService = retrofit.create(
-        ReadSurahEnService::class.java)
 }
