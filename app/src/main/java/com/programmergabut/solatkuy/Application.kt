150bfc5b9e57199b372a3e00a8a36fe29857a46b
JoeMobarak <mobarak.joe@gmail.com>
2021-10-18 14:34:13 +0300

    Starting koin in application class

--- app/src/main/java/com/programmergabut/solatkuy/Application.kt
+++ app/src/main/java/com/programmergabut/solatkuy/Application.kt
@@ -5,16 +5,36 @@ import android.content.Context
 import androidx.multidex.MultiDex
 import androidx.work.Configuration
 import com.programmergabut.solatkuy.data.local.SolatKuyRoom
+import com.programmergabut.solatkuy.di.*
 import com.programmergabut.solatkuy.worker.MyWorkerFactory
-import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
-import androidx.work.DelegatingWorkerFactory
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.fragment.koin.fragmentFactory
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
-@HiltAndroidApp
+
+
 class Application : Application(), Configuration.Provider {
 
-    @Inject
+
     lateinit var db: SolatKuyRoom
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@Application)
+            fragmentFactory()
+            modules(
+                listOf(
+                    databaseModule, networkModule, repositoryModule, sharedPrefModule,
+                    viewModelModule
+                )
+            )
+
+        }
+    }
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
@@ -24,7 +44,6 @@ class Application : Application(), Configuration.Provider {
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder()
             .setMinimumLoggingLevel(android.util.Log.DEBUG)
-            .setWorkerFactory(MyWorkerFactory(db.notifiedPrayerDao(), db.msApi1Dao()))
             .build()
     }
 
