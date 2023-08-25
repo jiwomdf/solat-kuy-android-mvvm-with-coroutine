a5368cc15f9cde76c0073649f40fb80c419b9aa9
Katili Jiwo <katili.jiwo@koinworks.com>
2023-08-25 16:02:12 +0700

    fastrata

--- app/src/test/java/com/programmergabut/solatkuy/CoroutineTestUtil.kt
+++
@@ -1,9 +0,0 @@
-package com.programmergabut.solatkuy
-
-import kotlinx.coroutines.CompletableDeferred
-
-class CoroutineTestUtil {
-    companion object {
-        fun <T> T.toDeferred() = CompletableDeferred(this)
-    }
-}
--- app/src/test/java/com/programmergabut/solatkuy/di/contextprovider/ContextProviderTest.kt
+++
@@ -1,9 +0,0 @@
-package com.programmergabut.solatkuy.di.contextprovider
-
-import kotlinx.coroutines.Dispatchers
-import kotlin.coroutines.CoroutineContext
-
-class ContextProviderTest: ContextProvider {
-    override fun main(): CoroutineContext = Dispatchers.Unconfined
-    override fun io(): CoroutineContext = Dispatchers.Unconfined
-}
